6e8432169cc6fe0a0241679f742f63726470c2a1
MUBUANGA Arnold <arnoldmubs@gmail.com>
2018-02-22 11:59:03 +0100

    RENDU TP3 GL

---
+++ TP_compilation/sayHello/compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+g++ -std=c++11 -Wall -Wextra -c module1.hpp module1.cpp
+g++ -std=c++11 -Wall -Wextra -c sayHello.cpp  
+g++ -std=c++11 -Wall -Wextra -o sayHello.out sayHello.o module1.o
